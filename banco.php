57a5c2d1f4c9141050d878dc6b33800a071da943
Sara Luz <saraluzaferreira.ifpr2024@gmail.com>
2026-05-20 08:04:19 -0300

    Add files via upload

---
+++ banco.php
@@ -0,0 +1,9 @@
+<?php
+$entidades = [
+   "nome"=>["aluno","professor","curso"]
+ ];
+$atributos = [
+    "aluno"=>["nome","idade","nascimento"],
+    "professor"=>["nome","idade","titulacao"],
+    "curso"=>["nome","turno","anos"],
+];
