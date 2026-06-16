8764f9d78ee6ae9f4b241aa11f1cd88931913f6c
Sara Luz <saraluzaferreira.ifpr2024@gmail.com>
2026-06-16 11:20:33 -0300

    Update index.php

--- criarClasse/index.php
+++ criarClasse/index.php
@@ -1,34 +1,25 @@
 <?php
-
-include("LeitorSQL.php");
-function criarClasse()
-{
-
-  if (is_dir("model")) {
-    mkdir("model", 0777, true);
-  }
-  $objSQL = new LeitorSQL("framework.sql");
-
-  $entidades = $objSQL->getTabelas();
-  foreach ($entidades as $entidade) {
-    $listarAtributos = $objSQL->getAtributos($entidade);
-    $attr = "";
-    $metodos = "";
-    foreach ($listarAtributos as $key => $atributo) {
-      $atr .= "private $" . $atributo . ";\n";
-      $metodos .= "function get" . ucfirst($key) . "(){\n";
-      $metodos .= "return \$this->" . $key . ";\n}\n";
-      $metodos .= "function set" . ucfirst($key) . "(\$arg)(\n";
-      $metodos .= "\$this->" . $key . "=\$arg;\n}\n";
+if(isset($_GET["erro"])){
+    switch ($_GET["erro"]){
+        case 0:
+            echo "<div class='erro'>Apenas arquivos SQL são permitidos.</div>";
+            break;
     }
-    $nomeClasse = ucfirst($entidade);
-    $conteudo = <<<CLASS
-
-<?php
-class $nomeClasse{
-$attr
-$metodos
-CLASS;
-    file_put_contents("model/ " . $entidade . ".php" . $conteudo);
-  } 
 }
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Framework MVC</title>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+<h1> Framework MVC</h1> <h2>Converta seu arquivo SQL em um sistema MVC completo</h2>
+<form action="LeitorSQL.php" method="post" enctype="multipart/form-data">
+    <label for="arquivo">Envie seu arquivo SQL</label>
+    <input type="file" name="arquivo" required><br>
+    <button type="submit">Enviar</button>
+
+</form>
+</body>
+</html>
