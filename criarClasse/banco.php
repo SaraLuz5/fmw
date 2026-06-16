5d4d914e66de2ed76d291419481a07ba73d567a4
Sara Luz <saraluzaferreira.ifpr2024@gmail.com>
2026-06-16 11:21:12 -0300

    Delete criarClasse/banco.php

--- criarClasse/banco.php
+++
@@ -1,9 +0,0 @@
-<?php
-$entidades = [
-   "nome"=>["aluno","professor","curso"]
- ];
-$atributos = [
-    "aluno"=>["nome","idade","nascimento"],
-    "professor"=>["nome","idade","titulacao"],
-    "curso"=>["nome","turno","anos"],
-];
