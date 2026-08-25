223f77090e8f1d541f8bd2e4d7f87dddd21b316d
Sara Luz <saraluzaferreira.ifpr2024@gmail.com>
2026-08-25 11:00:58 -0300

    Add files via upload

---
+++ fwkAlterar/sistema/control/professorControl.php
@@ -0,0 +1,46 @@
+<?PHP
+require_once('../model/professor.php');
+ require_once('../dao/professorDAO.php');
+class ProfessorControl {
+   private $obj;
+   private $dao;
+   private $acao;
+   public function __construct() {
+       $this->obj=new Professor();
+       $this->dao=new ProfessorDAO();
+       $this->acao=$_REQUEST["acao"] ?? null;
+      $this->executaAcao();
+   }
+   public function executaAcao() {
+   switch($this->acao) {
+          case 1:
+          $this->prepararObjeto();
+          $this->dao->inserir( $this->obj);
+          break;
+          case 2:
+          return $this->dao->listar();
+          case 3:
+          $this->dao->excluir( $_REQUEST["id"] );
+          break;
+          case 4:
+          $obj=$this->dao->buscarPorId( $_REQUEST["id"] );
+          require_once("../view/form_professor.php");
+          break;
+          case 5:
+          $this->prepararObjeto();
+          $this->dao->alterar( $this->obj);
+          break;
+      }
+   }
+   public function prepararObjeto() {
+      $this->obj->setNome($_POST["nome"]);
+	$this->obj->setEmail($_POST["email"]);
+	$this->obj->setEspecialidade($_POST["especialidade"]);
+	$this->obj->setData_admissao($_POST["data_admissao"]);
+	if(isset($_POST["id_professor"]))
+	$this->obj->setId_professor($_POST["id_professor"]);
+	
+   }
+}
+new ProfessorControl;
+?>
---
+++ fwkAlterar/sistema/model/Conexao.php
@@ -0,0 +1,20 @@
+<?php
+class Conexao{
+    public static function conectar():PDO{
+        try {
+            $host = "127.0.0.1";
+            $banco = "aluno";
+            $usuario = "root";
+            $senha = "bancodedados";
+            $pdo = new PDO(
+                "mysql:host=$host;dbname=$banco;charset=utf8",
+                $usuario,
+                $senha
+            );
+            return $pdo;
+        } catch (PDOException $e) {
+           echo "Erro na conexão: " . $e->getMessage();
+        }
+    }
+}
+?>
